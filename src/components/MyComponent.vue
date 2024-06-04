411d75f3eb96de56f418960228a4236e69d0b29a
Wahan Havhannesyan <Wahan2500@hotmail.com>
2024-06-04 10:09:20 +0200

    Made component

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p v-if="isTrue">The value is true!</p>
+    <p v-else>The value is false!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isTrue: true,
+    };
+  },
+};
+</script>
